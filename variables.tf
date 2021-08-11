2f6c05e1df3981992d6a1b1bbdad1c5055f9eb5c
Emmanuel Eliason-Armstrong <geteliarms@gmail.com>
2021-08-11 12:58:59 -0400

    feat(network) : Deploy VPCs, Internet GWs, and Subnets

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "profile" {
-    type = string
-    default = "default" 
+  type    = string
+  default = "default"
 }
 
-variable "region-master"{
-    type = string
-    default = "us-east-1"
+variable "region-master" {
+  type    = string
+  default = "us-east-1"
 }
-variable "region-worker"{
-    type = string
-    default = "us-east-2"
+variable "region-worker" {
+  type    = string
+  default = "us-east-2"
 }
